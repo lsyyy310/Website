74dd5ccdc723b7d7ac151a853f841ac386b60157
Lin Shu You <lsyyy310@gmail.com>
2023-08-04 20:35:16 +0800

    Update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 async function preload () {
-  let preloader = document.getElementById("preloader");
+  const preloader = document.getElementById("preloader");
   preloader.innerHTML = `
       <div id="curtain"></div>
       <div id="ring"></div>
@@ -11,15 +11,14 @@ async function preload () {
     })
   }
 
-  // draw the curtains
+  const head = document.head,
+        ring = document.getElementById("ring"),
+        ringWidth = getComputedStyle(ring).width.slice(0, -2);
   function drawCurtains () {
-    const animationDuration = 1;
-    return sleep(animationDuration * 1.5)
+    const rotationDuration = getComputedStyle(ring, "::before").animationDuration.slice(0, -1);
+    return sleep(rotationDuration * 1.5)
   }
 
-  let head = document.head;
-      ring = document.getElementById("ring"),
-      ringWidth = getComputedStyle(ring).width.slice(0, -2);
   function setFontSize () {
     if (ringWidth == 104) {
       return "16px"
@@ -41,9 +40,38 @@ async function preload () {
 }
 // End function preload
 
-document.addEventListener("DOMContentLoaded", function () {
-  preload();
-  let pageBottom = document.getElementById("page__bottom");
-  pageBottom.setAttribute("date", "Aug 2, 2023");
-});
+function writeHTML () {
+  const currentUrl = location.pathname,
+        bottomExclusion = ["/index.html"],
+        navBar = document.getElementById("navbar");
+  navBar.innerHTML = `
+    <nav id="navcontent--right">
+      <ul>
+        <li><a href="./index.html">HOME</a></li>|
+        <li><a href="./about.html">ABOUT</a></li>|
+        <li><a href="./project.html">PROJECT</a></li>|
+        <li><a href="./contact.html">CONTACT</a></li>
+      </ul>
+    </nav>
+  `
+  for (url of bottomExclusion) {
+    if (currentUrl.includes(url) === false) {
+      const pageBottom = document.getElementById("page__bottom");
+      pageBottom.setAttribute("date", "Aug 3, 2023");
+      break
+    }
+  }
+  // in index.html
+  if (currentUrl.includes("/index.html") === true) {
+    const menuIcon = document.getElementById("menu__icon");
+    menuIcon.innerHTML = `
+      <div class="line"></div>
+      <div class="line"></div>
+      <div class="line"></div>
+    `
+  }
+}
+
+
+document.addEventListener("DOMContentLoaded", () => {preload(); writeHTML();});
 // document.addEventListener("load", )
