9383cd71b157dc5f3ff0956d65bcbf8b4f73b177
Lin Shu You <lsyyy310@gmail.com>
2023-08-27 19:22:07 +0800

    Update

--- js/script.js
+++ js/script.js
@@ -23,48 +23,44 @@ async function preload () {
     if (ringWidth == 104) {
       return "16px"
     } else {
-      return `${ringWidth / 7}px`
+      return `${Math.round(ringWidth / 7)}px`
     }
   }
 
   const fontSize = setFontSize();
   head.innerHTML += `
-  <style>
+  <style id="preloader__style">
     #ring::after {
       font-size: ${fontSize};
     }
   </style>
   `;
+  const preloaderStyle = document.getElementById("preloader__style");
   await drawCurtains();
   preloader.remove();
+  preloaderStyle.remove();
+  // preloaderStyle.remove();
 }
 // End function preload
 
 function writeHTML () {
   const currentUrl = location.pathname,
         bottomExclusion = ["/index.html"],
-        navBar = document.getElementById("navbar");
-  navBar.innerHTML = `
-    <nav id="navcontent--right">
-      <ul>
-        <li><a href="./index.html">HOME</a></li>|
-        <li><a href="./about.html">ABOUT</a></li>|
-        <li><a href="./project.html">PROJECT</a></li>|
-        <li><a href="./contact.html">CONTACT</a></li>
-      </ul>
-    </nav>
-  `
+        navBar = document.getElementById("nav-bar");
+      navBar.innerHTML = `
+        <nav id="navcontent--right">
+            <a class="menu-item" href="./index.html">HOME</a>
+            <a class="menu-item" href="./about.html">ABOUT</a>
+            <a class="menu-item" href="./project.html">PROJECT</a>
+            <a class="menu-item" href="./contact.html">CONTACT</a>
+        </nav>
+      `
   for (url of bottomExclusion) {
     if (currentUrl.includes(url) === false) {
       const pageBottom = document.getElementById("page__bottom");
-      pageBottom.setAttribute("date", "Aug 3, 2023");
+      pageBottom.setAttribute("date", "Aug 26, 2023");
       break
     }
-  }
-  // in index.html
-  if (currentUrl.includes("/index.html") === true) {
-    const menuIcon = document.getElementById("menu__icon");
-    menuIcon.innerHTML = `<div class="line"></div>`.repeat(3)
   }
 }
 
